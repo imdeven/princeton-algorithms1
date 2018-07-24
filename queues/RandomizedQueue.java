1de55c0b293f78eab2be9778d4766c8512acce73
devendra.mandan <devendra.mandan@flipkart.com>
2018-07-24 13:03:21 +0530

    copied api definition for randomized queue

---
+++ queues/RandomizedQueue.java
@@ -0,0 +1,48 @@
+import java.util.Iterator;
+
+/**
+ * Created by devendra.mandan on 23/07/18.
+ */
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private int size;
+
+    public RandomizedQueue()                 // construct an empty randomized queue
+    {
+    }
+
+    public boolean isEmpty()                 // is the randomized queue empty?
+    {
+        return true;
+    }
+
+    public int size()                        // return the number of items on the randomized queue
+    {
+        return size;
+    }
+
+    public void enqueue(Item item)           // add the item
+    {
+
+    }
+
+    public Item dequeue()                    // remove and return a random item
+    {
+
+    }
+
+    public Item sample()                     // return a random item (but do not remove it)
+    {
+    }
+
+    public Iterator<Item> iterator()         // return an independent iterator over items in random order
+    {
+
+    }
+
+    public static void main(String[] args)   // unit testing (optional)
+    {
+
+    }
+
+}
