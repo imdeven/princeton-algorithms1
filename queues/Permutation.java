9a14b7df89be8f4c5d2738bc0b2da535d7d031cc
devendra.mandan <devendra.mandan@flipkart.com>
2018-07-26 00:00:42 +0530

    implemented permutation

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -1,5 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+
 /**
  * Created by devendra.mandan on 25/07/18.
  */
 public class Permutation {
+    public static void main(String[] args) {
+
+        int k = StdIn.readInt();
+
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
+
+        while (!StdIn.isEmpty()) {
+            randomizedQueue.enqueue(StdIn.readString());
+        }
+
+        while (k > 0) {
+            System.out.println(randomizedQueue.dequeue());
+            k--;
+        }
+    }
 }
