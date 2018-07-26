904c81233539fada9eb430a8a509a817f58d0987
devendra.mandan <devendra.mandan@flipkart.com>
2018-07-26 10:44:25 +0530

    taking k as command line argument rather from stdin

--- queues/Permutation.java
+++ queues/Permutation.java
@@ -6,7 +6,7 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
     public static void main(String[] args) {
 
-        int k = Integer.parseInt(StdIn.readString());
+        int k = Integer.parseInt(args[0]);
 
         RandomizedQueue<String> randomizedQueue = new RandomizedQueue<>();
 
